44407a48fadea399c6e01259d7b5f9639cc3d6f2
mvk-mozilla <mvankleeck@mozilla.com>
2018-12-12 13:17:30 -0800

    Initial implementation

---
+++ src/ExampleLambda.py
@@ -0,0 +1,14 @@
+import json
+
+def lambda_handler(event, context):
+    print("Hi CLoudWatch!")
+    print ("I am a handler")
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('I got a body for u'), #err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+        #'body': json.dumps('Hello from Lambda!')
+    }
