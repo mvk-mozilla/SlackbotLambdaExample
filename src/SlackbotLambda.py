9a3975d308263781f6499c63d8233b4fe0768d4b
mvk-mozilla <mvankleeck@mozilla.com>
2018-12-14 15:07:04 -0800

    Added some code for testing. Started to work on some imports but not yet ready to deploy that, so it's commented out.

--- src/SlackbotLambda.py
+++ src/SlackbotLambda.py
@@ -1,14 +1,22 @@
 import json
+#import nltk
+#import numpy as np
 
 def lambda_handler(event, context):
     print("Hi CLoudWatch!")
     print ("I am a handler")
+    text = event.get('text', 'No event given')
+#    tokenized = nltk.word_tokenize(text)
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('I got a body for u and it comes from an S3 ZIP!!!\n Plus I\'ve been testing'), #err.message if err else json.dumps(res),
+        'body': json.dumps('I got a body for u and it comes from an S3 ZIP!!!\n Plus I\'ve been testing. My event is '+text), #err.message if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
         },
         #'body': json.dumps('Hello from Lambda!')
     }
+
+if __name__ == "__main__":
+
+    lambda_handler({'text':'event test'}, '')
